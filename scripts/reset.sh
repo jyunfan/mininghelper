68bad12720532d8d82e66b7594db6b57d96b3bad
Jyun-Fan Tsai <jyunfan@gmail.com>
2013-05-12 15:26:45 +0800

    Fix evaluation problem.

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -11,7 +11,7 @@ do
   # Get metrics of all devices
   RATE=$(echo -n 'devs' | nc ${IP} ${PORT} | egrep -a -o 'MHS 5s=[0-9]+' | egrep -a -o '[0-9]+$')
   # Get the specific metric we want
-  RATE=echo $RATE | awk "{print "'$'"$DEVICE}"
+  RATE=$(echo $RATE | awk "{print "'$'"$DEVICE}")
   echo "RATE=$RATE" $(date)
   if [ $RATE -lt $THRESH ]; then
     echo "Reset" $(date)
